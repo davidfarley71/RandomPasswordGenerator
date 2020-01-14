e06ef1b682680a29f455852ed3c41cb861477689
Drubaloo <Drubaloo@outlook.com>
2020-01-13 21:11:07 -0800

    password generation success

--- main.js
+++ main.js
@@ -13,19 +13,59 @@ var options = {
     lower: lowerCaseLetters,
     upper: upperCaseLetters,
     number: randomNumber,
-    symbol: randomSymbol 
+    symbol: randomSymbol
 }
+
+//button function
+
+createElement.addEventListener('click', () => {
+    var length = parseInt(lengthElement.value)
+    var isLower = lowerElement.checked
+    var isUpper = upperElement.checked
+    var isNumber = numberElement.checked
+    var isSymbol = symbolElement.checked
+
+    resultElement.innerText = createPassword(isLower, isUpper, isNumber, isSymbol, length)
+})
+
+//copy to clipboard
+
+
 
 //create password
 
-createElement.addEventListener('click', () => {
-    var length = lengthElement.value
-    
-    console.log(length)
-})
+function createPassword(lower, upper, number, symbol, length) {
+    // initilize password variable
+    // filter out unchecked types
+    // loop over length call generator function for each type
+    //add final password to the password variable and return it
 
+    var createPassword = ''
 
+    var checkedCount = lower + upper + number + symbol
 
+    //console.log(checkedCount)
+
+    var checkedArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0])
+
+    //console.log('arrtype:', checkedArr)
+
+    if(checkedCount === 0){
+        return 'please select an option'
+    }
+
+    for (let i = 0; i < length; i += checkedCount){
+        checkedArr.forEach(type => {
+            var optionName = Object.keys(type)[0]
+            //console.log(optionName)
+
+            createPassword += options[optionName]();
+        });
+    }
+
+    var fullPassword = createPassword.slice(0, length)
+    return fullPassword
+}
 
 // option functions
 
@@ -33,15 +73,15 @@ function lowerCaseLetters() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 
-function upperCaseLetters () {
+function upperCaseLetters() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
 }
 
-function randomNumber () {
+function randomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48)
 }
 
-function randomSymbol () {
+function randomSymbol() {
     var symbols = '!@#$%^&*()'
     return symbols[Math.floor(Math.random() * symbols.length)]
 }
